196d3546e5377f1969f42611bf57e40091e35fbe
Showlin Akter <showlinakter77@gmail.com>
2025-01-05 00:19:25 +0600

    create a login page

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBcJqUGMw4KJEQNVxOA8eyXZnCt1zAfe-A",
+    authDomain: "hotel-fair-d7634.firebaseapp.com",
+    projectId: "hotel-fair-d7634",
+    storageBucket: "hotel-fair-d7634.firebasestorage.app",
+    messagingSenderId: "473445573524",
+    appId: "1:473445573524:web:2bbf697f20c16e2086a581"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default  app;
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Component/Routes/Routes';
+import AuthProvider from './AuthProvider/AuthProvider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
