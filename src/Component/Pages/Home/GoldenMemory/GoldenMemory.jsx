5df359e6d021f64a7ae9324f8b31c3d622128372
Showlin Akter <showlinakter77@gmail.com>
2025-01-06 11:50:46 +0600

    create a golden memory section

---
+++ src/Component/Pages/Home/GoldenMemory/GoldenMemory.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GoldenMemory = () => {
+    return (
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default GoldenMemory;
