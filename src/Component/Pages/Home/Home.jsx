cb03411994f3ce186c33850a0d25516d7f7a2a7a
Showlin Akter <showlinakter77@gmail.com>
2025-01-06 10:35:23 +0600

    create a Promotion Section

--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 
 import Banner from "./Banner/Banner";
 import Card from "../../Card/Card";
+import { Map, Marker } from "pigeon-maps";
 
 const Home = () => {
     return (
@@ -22,6 +23,24 @@ const Home = () => {
                 </div>
                 <div>
                     <Card></Card>
+                </div>
+            </div>
+            <div className="pt-24">
+                <div className="pb-10">
+                    <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl">
+                        Our Location
+                    </h1>
+
+                    <div className="flex justify-center mx-auto mt-6">
+                        <span className="inline-block w-40 h-1 bg-rose-400 rounded-full"></span>
+                        <span className="inline-block w-3 h-1 mx-1 bg-rose-400 rounded-full"></span>
+                        <span className="inline-block w-1 h-1 bg-rose-400 rounded-full"></span>
+                    </div>
+                </div>
+                <div className="px-3">
+                    <Map height={400} defaultCenter={[23.8053, 90.3589]} defaultZoom={11}>
+                        <Marker width={50} anchor={[23.8053, 90.3589]} />
+                    </Map>
                 </div>
             </div>
         </div>
---
+++ src/Component/Promotion/Promotion.jsx
@@ -0,0 +1,11 @@
+
+
+const Promotion = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Promotion;
